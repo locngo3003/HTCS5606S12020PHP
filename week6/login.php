36577fe8348f6b97b3d610adf6e22a8db61df48c
locngo3003 <locnxse04549@gmail.com>
2020-05-26 09:56:34 +1200

    login

---
+++ week6/login.php
@@ -0,0 +1,32 @@
+<?php
+if (isset($_POST["username"])) {
+    $username = $_POST["username"];
+    $password = $_POST["password"];
+
+
+    $server = "kil9uzd3tgem3naa.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
+    $user = "eihuxghz1d16wln9";
+    $pwd = "rm6ssod1ra1y6iho";
+    $database = "xuc77vapd06kjxop";
+
+    $connection = new mysqli($server, $user, $pwd, $database); //create database connection
+    if ($connection->connect_error) {
+        echo $connection->connect_error;
+    } else {
+        echo "Connection Created";
+    }
+//is the username in my table
+    $sql = "select +from Users where  usename = '$username' ";
+    $result = $connection->query($sql);
+    if ($result->num_rows == 1) {
+        while ($row = $result->fetch_assoc()) {
+            if ($row["password"] == $password) {
+                echo 'access';
+            } else {
+                echo 'wrong password';
+            }
+        }
+    } else {
+        echo "wrong username";
+    }
+}
